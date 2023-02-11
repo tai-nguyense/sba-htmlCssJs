6e874c710907fd9644adb99cbb46f309d671a396
tai-nguyense <118483867+tai-nguyense@users.noreply.github.com>
2023-02-11 15:48:47 -0600

    add new task function

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const taskForm = document.querySelector("#taskForm")
 const taskBtn = document.querySelector("#taskBtn");
 const taskInput = document.querySelector("#taskInput");
+const taskList = document.querySelector("#taskList");
+
 
 taskForm.addEventListener("onclick", function(e){
     e.preventDefault()
 })
 taskBtn.addEventListener("onclick", function(){
-    alert(taskText.value)
+    addNewTask()
 })
+function addNewTask(){
+    const newP = document.createElement("p")
+    newP.textContent = taskInput.value
+    const newBtn = document.createElement("button")
+    newBtn.textContent = "-"
+    const newLi = document.createElement("li")
+    newLi.appendChild(newP)
+    newLi.apenndChild(newBtn)
+    taskList.appendChild(newLi)
+}
