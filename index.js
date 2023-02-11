7679580230ee311aef94d8365bcb59eea23df2d1
tai-nguyense <118483867+tai-nguyense@users.noreply.github.com>
2023-02-11 15:13:20 -0600

    add task form and task table

---
+++ index.js
@@ -0,0 +1,6 @@
+const taskBtn = document.querySelector("#taskBtn");
+const taskInput = document.querySelector("#taskInput");
+
+taskBtn.addEventListener("onclick", function(){
+    alert("clicked")
+});
