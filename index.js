23c3b9543565a6238d924dbafdc175f06a6d2db3
tai-nguyense <118483867+tai-nguyense@users.noreply.github.com>
2023-02-11 15:22:27 -0600

    add form preventDefault

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const taskForm = document.querySelector("#taskForm")
 const taskBtn = document.querySelector("#taskBtn");
 const taskInput = document.querySelector("#taskInput");
 
+taskForm.addEventListener("onclick", function(e){
+    e.preventDefault();
+})
 taskBtn.addEventListener("onclick", function(){
     alert("clicked")
 });
