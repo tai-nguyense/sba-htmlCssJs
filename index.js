eadf9f682ff4a1394a4a9b94ae57d4a8df07f6dd
tai-nguyense <118483867+tai-nguyense@users.noreply.github.com>
2023-02-11 20:08:23 -0600

    add google font kanit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const taskForm = document.querySelector("#taskForm")
+const taskForm = document.querySelector("#taskForm");
 const taskBtn = document.querySelector("#taskBtn");
 const taskInput = document.querySelector("#taskInput");
 const taskList = document.querySelector("#taskList");
 
 taskBtn.addEventListener("onclick", function(){
     taskBtn.style.backgroundColor = red;
-    const newTd = document.createElement("td")
-    newTd.textContent = taskInput
-    const newBtn = document.createElement("button")
-    newBtn.setAttribute("type","button")
-    newBtn.textContent = "+"
-    newBtn.classList.add("btn")
-    const newTr = document.createElement("tr")
-    taskList.appendChild(newTr)
-    newTr.appendChild(newTd)
-    newTr.appendChild(newBtn)
-})
+    const newTd = document.createElement("td");
+    newTd.textContent = taskInput;
+    const newBtn = document.createElement("button");
+    newBtn.setAttribute("type","button");
+    newBtn.textContent = "+";
+    newBtn.classList.add("btn");
+    const newTr = document.createElement("tr");
+    taskList.appendChild(newTr);
+    newTr.appendChild(newTd);
+    newTr.appendChild(newBtn);
+});
