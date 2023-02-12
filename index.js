4ffe9ac07b57aeb63d72899bd26554e68eafa7c3
tai-nguyense <118483867+tai-nguyense@users.noreply.github.com>
2023-02-11 21:40:43 -0600

    add delete task function

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ const taskForm = document.querySelector("#taskForm");
 const taskBtn = document.querySelector("#taskBtn");
 const taskInput = document.querySelector("#taskInput");
 const taskList = document.querySelector("#taskList");
+const allDeleteBtns = document.querySelector(".deleteBtn")
+
 taskForm.addEventListener("submit", function(e){
     e.preventDefault();
 });
 taskBtn.addEventListener("click", function(){
     taskText = taskInput.value.trim()
-    if(taskText = ""){
+    if(taskText.length===0){
         return;
     }
     const newTd = document.createElement("td");
     newTd.textContent = taskText;
     const newBtn = document.createElement("button");
     newBtn.setAttribute("type","button");
-    newBtn.textContent = "+";
-    newBtn.classList.add("btn");
+    newBtn.textContent = "-";
+    newBtn.classList.add("btn deleteBtn");
     const newTr = document.createElement("tr");
     taskList.appendChild(newTr);
     newTr.appendChild(newTd);
     newTr.appendChild(newBtn);
     taskInput.value = "";
 });
+for(const delBtn of allDeleteBtns){
+    delBtn.addEventListener("click", function(){
+        liToDel = delBtn.parentNode;
+        liToDel.parentNode.removeChild(liToDel);
+    })
+}
