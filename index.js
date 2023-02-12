03444dca8cf62aedca8fcadce9ead3b47ab53341
Tai Nguyen <tai.minise@gmail.com>
2023-02-12 17:58:10 -0600

    working task list

--- index.js
+++ index.js
@@ -4,30 +4,37 @@ const taskInput = document.querySelector("#taskInput");
 const taskList = document.querySelector("#taskList");
 taskForm.addEventListener("submit", function(e) {
     e.preventDefault();
+    addTask();
 });
 taskBtn.addEventListener("click", function() {
+    addTask();
+});
+
+function addTask() {
     taskText = taskInput.value.trim();
     if (taskText.length < 1) {
         return;
     }
     const newTd = document.createElement("td");
-    newTd.classList.add("row")
-    newTd.style.display = "flex";
+    newTd.classList.add("col-10")
     const newP = document.createElement("span");
     newP.textContent = taskInput.value;
-    newP.classList.add("col-11");
+    const newTdBtn = document.createElement("td");
+    newTdBtn.classList.add("col-2")
     const newBtn = document.createElement("button");
     newBtn.setAttribute("type", "button");
     newBtn.textContent = "-";
-    newBtn.classList.add("btn", "deleteBtn", "col-1");
+    newBtn.classList.add("btn");
     addDelete(newBtn);
     const newTr = document.createElement("tr");
+    newTr.classList.add("row");
     taskList.appendChild(newTr);
     newTr.appendChild(newTd);
+    newTr.appendChild(newTdBtn);
     newTd.appendChild(newP);
-    newTd.appendChild(newBtn);
+    newTdBtn.appendChild(newBtn);
     taskInput.value = "";
-});
+}
 
 function addDelete(delBtn) {
     delBtn.addEventListener("click", function() {
