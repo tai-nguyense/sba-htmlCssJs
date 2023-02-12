fcf9becca1d4266cd1a68d93f6afc4c6f278c589
Tai Nguyen <tai.minise@gmail.com>
2023-02-12 12:02:28 -0600

    add task delete button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const taskForm = document.querySelector("#taskForm");
 const taskBtn = document.querySelector("#taskBtn");
 const taskInput = document.querySelector("#taskInput");
 const taskList = document.querySelector("#taskList");
+const allDeleteBtns = document.querySelector(".deleteBtn");
 taskForm.addEventListener("submit", function(e) {
     e.preventDefault();
 });
@@ -14,11 +15,19 @@ taskBtn.addEventListener("click", function() {
     newTd.textContent = taskInput.value;
     const newBtn = document.createElement("button");
     newBtn.setAttribute("type", "button");
-    newBtn.textContent = "+";
-    newBtn.classList.add("btn");
+    newBtn.textContent = "-";
+    newBtn.classList.add("btn", "deleteBtn");
+    addDelete(newBtn);
     const newTr = document.createElement("tr");
     taskList.appendChild(newTr);
     newTr.appendChild(newTd);
     newTr.appendChild(newBtn);
     taskInput.value = "";
 });
+
+function addDelete(delBtn) {
+    delBtn.addEventListener("click", function() {
+        const tr = delBtn.parentNode;
+        tr.parentNode.removeChild(tr);
+    })
+}
