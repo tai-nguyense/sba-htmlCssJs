07674f55f82d63cd67ee9b0b1340c391acf3dff2
Tai Nguyen <tai.minise@gmail.com>
2023-02-13 00:08:44 -0600

    working up to 10 task list

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const taskInput = document.querySelector("#taskInput");
 const taskList = document.querySelector("#taskList");
 const navToggler = document.querySelector(".navbar-toggler");
 const navIcon = document.querySelector(".nav-icon");
-const wheelBtn = document.querySelector("#wheelSpinnerBtn");
-const wheel = document.querySelector(".wheelContainer");
+const refreshWheelBtn = document.querySelector("#refreshWheelBtn");
+const wheelSpinnerBtn = document.querySelector("#wheelSpinnerBtn");
+const wheelContainer = document.querySelector(".wheelContainer");
 
 navToggler.addEventListener("click", function() {
     navIcon.classList.toggle("open");
@@ -17,19 +18,20 @@ taskForm.addEventListener("submit", function(e) {
 taskBtn.addEventListener("click", function() {
     addTask();
 });
-wheelBtn.addEventListener("click", function() {
-    spinWheel();
-})
 
 function addTask() {
     taskText = taskInput.value.trim();
     if (taskText.length < 1) {
+        return;
+    } else if (getTaskArray().length >= 10) {
+        alert("Task List reached max size of 10 already.")
         return;
     }
     const newTd = document.createElement("td");
     newTd.classList.add("col-10")
     const newP = document.createElement("span");
     newP.textContent = taskInput.value;
+    newP.classList.add("taskText")
     const newTdBtn = document.createElement("td");
     newTdBtn.classList.add("col-2")
     const newBtn = document.createElement("button");
@@ -55,9 +57,47 @@ function addDelete(delBtn) {
     })
 }
 
+function getTaskArray() {
+    var taskArray = [];
+    $('.taskText').each(function() {
+        taskArray.push(this.textContent);
+    })
+    return taskArray;
+}
+
+refreshWheelBtn.addEventListener("click", function() {
+    var taskArray = getTaskArray();
+    refreshWheel(taskArray);
+})
+
+function refreshWheel(taskArray) {
+    var wheelTask = wheelContainer.lastElementChild;
+    while (wheelTask) {
+        wheelContainer.removeChild(wheelTask);
+        wheelTask = wheelContainer.lastElementChild;
+    }
+    if (taskArray.length === 0) {
+        return;
+    }
+    var counter = 0;
+    spinWheel();
+    $(taskArray).each(function() {
+        counter++;
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("wheelTask");
+        newDiv.textContent = counter;
+        var xDeg = 360 / taskArray.length;
+        newDiv.style.transform = "rotate(" + xDeg * counter + "deg)";
+        wheelContainer.append(newDiv);
+    })
+}
+
+wheelSpinnerBtn.addEventListener("click", function() {
+    spinWheel();
+})
 var randomDeg = 360 * 3 + Math.floor(Math.random() * 360 * 2);
 
 function spinWheel() {
     randomDeg += 360 * 3 + Math.floor(Math.random() * 360 * 2);
-    wheel.style.transform = "rotate(" + randomDeg + "deg)";
+    wheelContainer.style.transform = "rotate(" + randomDeg + "deg)";
 }
